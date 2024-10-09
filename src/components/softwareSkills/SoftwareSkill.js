b43cb3c42797f912182dc0410dcbf16eacd6feb5
Agnese <s305900@studenti.polito.it>
2024-10-09 17:41:33 +0200

    vediam

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -14,7 +14,7 @@ export default function SoftwareSkill() {
                 className="software-skill-inline"
                 name={skills.skillName}
               >
-                <i className={skills.fontAwesomeClassname}></i>
+                <img src={skills.svgIcon} alt={`${skills.skillName} icon`} className="svg-icon" />
                 <p>{skills.skillName}</p>
               </li>
             );
